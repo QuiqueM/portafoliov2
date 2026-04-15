ef3724540a33378436a31ee5dc618ec5bfea37f5
Enrique Martinez <m_r_enrique@outlook.com>
2026-04-14 18:49:06 -0600

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://portafoliov2-eight.vercel.app',
+  integrations: [sitemap()]
+});
